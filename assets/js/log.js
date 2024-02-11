312a24c1260477636e6874b83d94405341dc2b5f
Musab Ali Alashwal <112972839+Musabalialashwal@users.noreply.github.com>
2024-02-12 01:30:22 +0300

    Add files via upload

---
+++ assets/js/log.js
@@ -0,0 +1,20 @@
+let backgroundImage = [
+    "color: black",
+    "padding: 100px",
+    "font-weight: bolder",
+    "font-size: 20px",
+    "-webkit-text-stroke-width: 1px",
+    "-webkit-text-stroke-color: yellow",
+    "text-transform: uppercase",
+    "text-align: center",
+    "letter-spacing: 1px",
+    "font-family: 'Times New Roman', serif",
+].join(" ;");
+    
+    console.warn
+    (
+        "%cانتبة! \n%c ميزة المتصفح هذه موجهة للمطورين. إذا طلب منك شخص ما نسخ ولصق أي شيء هنا لتمكين ميزة على دوينج أو 'اختراق' حساب شخص آخر، فإن هذه محاولة احتيالية وستتيح له الوصول إلى حساب دوينج الخاص بك.", 
+        "color: red; font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;" , 
+        backgroundImage
+    );
+
